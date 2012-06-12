2aa5b44ee54b63183aa314870ceee4deb49b7a84
Dean Del Ponte <dean.delponte@gmail.com>
2012-06-11 21:47:15 -0500

    Adding domain classes and controllers

--- grails-app/domain/com/merrycoders/furthercms/Category.groovy
+++ grails-app/domain/com/merrycoders/furthercms/Category.groovy
@@ -1,7 +1,12 @@
 package com.merrycoders.furthercms
 
 class Category {
+    Category parent
+    String urlKey
+    Page page
 
     static constraints = {
+        parent(nullable: true)
+        page(nullable: true)
     }
 }
