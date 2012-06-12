d7c87ebff19589188a551bc1a3e1d0ac7866ab51
Dean Del Ponte <dean.delponte@gmail.com>
2012-06-11 21:59:24 -0500

    Adding domain classes and controllers

--- grails-app/domain/com/merrycoders/furthercms/Category.groovy
+++ grails-app/domain/com/merrycoders/furthercms/Category.groovy
@@ -1,6 +1,7 @@
 package com.merrycoders.furthercms
 
 class Category {
+    String name = ""
     Category parent
     String urlKey
     Page page
@@ -8,5 +9,30 @@ class Category {
     static constraints = {
         parent(nullable: true)
         page(nullable: true)
+    }
+
+    /**
+     *
+     * @return List of immediate children
+     */
+    List<Category> getChildren() {
+        if (this?.id) Category.findAllByParent(this, [sort: "name"])
+        else return []
+    }
+
+    /**
+     *
+     * @return List of all children at all levels
+     */
+    List<Category> getDeepChildren() {
+        def list = []
+
+        def children = getChildren()
+        list.addAll(children)
+
+        children.each { child ->
+            list.addAll(child.getDeepChildren())
+        }
+        return list
     }
 }
